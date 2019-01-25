83eb8039bef54194bb674fac3e526fc2e4df541b
thomas <iagtkuhn@iagpc151.iag.uni-stuttgart.de>
2019-01-25 13:12:41 +0100

    Vreman model

--- src/equations/navierstokes/eddyVisc/eddyVisc_vars.f90
+++ src/equations/navierstokes/eddyVisc/eddyVisc_vars.f90
@@ -54,5 +54,6 @@ REAL              :: PrSGS                 !< Prandtl number for the sub-grid sc
 LOGICAL           :: VanDriest=.FALSE.     !< Logical indicating if Van Driest damping is activated (only use for channel flow)
 LOGICAL           :: SmagorinskyInitIsDone=.FALSE. !< Logical indicating if smagorinsky model has been initialized
 LOGICAL           :: SigmaModelInitIsDone=.FALSE.  !< Logical indicating if sigma model has been initialized
+LOGICAL           :: VremanInitIsDone=.FALSE.      !< Logical indicating if sigma model has been initialized
 
 END MODULE MOD_EddyVisc_Vars

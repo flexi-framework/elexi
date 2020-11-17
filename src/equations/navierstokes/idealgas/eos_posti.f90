9b1f1ca8e894177dbb2a5244cc2e50f365e10592
Patrick Kopper <patrick.kopper@ila.uni-stuttgart.de>
2020-11-17 16:15:31 +0100

    The NodeCoords for the NonDimensionalGridSpacing get calculated on PP_N but evaluated on NGeo
    Furthermore, we do not need to calculate the NodeCoords at all since they are already available from the mesh file

--- src/equations/navierstokes/idealgas/eos_posti.f90
+++ src/equations/navierstokes/idealgas/eos_posti.f90
@@ -643,7 +643,7 @@ SUBROUTINE FillNonDimensionalGridSpacing(nSides_calc,mapBCSideToVisuSides,Nloc,d
 USE MOD_Eos_Vars
 USE MOD_Preproc
 USE MOD_Viscosity
-USE MOD_Mesh_Vars           ,ONLY: NGeo,Elem_xGP,SideToElem,nBCSides
+USE MOD_Mesh_Vars           ,ONLY: NGeo,Elem_xGP,SideToElem,nBCSides,NodeCoords
 USE MOD_Interpolation       ,ONLY: GetVandermonde
 USE MOD_Interpolation_Vars  ,ONLY: NodeType,NodeTypeGL
 USE MOD_ChangeBasisByDim    ,ONLY: ChangeBasisVolume
@@ -666,15 +666,12 @@ REAL,INTENT(OUT)                                     :: wallDistance(0:Nloc,0:ZD
 REAL              :: mu,temp,fricVel
 INTEGER           :: iSide,p,q,iSideVisu,ElemID,locSideID
 REAL              :: xVec(3),yVec(3),yloc(3),tVec(3,2)
-REAL              :: NodeCoords(3,0:PP_N,0:PP_N,0:PP_NZ)
 REAL              :: Vdm_G_GL(0:PP_N,0:PP_N)
 #if PP_dim==3
 INTEGER           :: i
 REAL              :: refVec(3),scalProd,scalProdMax,zVec(3)
 #endif
 !===================================================================================================================================
-! We need a Vandermonde matrix to get the GP coordinates on GL points, i.e. including the surface of the grid cells
-CALL GetVandermonde(PP_N,NodeType,PP_N,NodeTypeGL,Vdm_G_GL)
 
 ! Loop over all boundary sides
 DO iSide=1,nBCSides
@@ -689,28 +686,25 @@ DO iSide=1,nBCSides
     ElemID        = SideToElem(S2E_ELEM_ID,iSide)
     locSideID     = SideToElem(S2E_LOC_SIDE_ID,iSide)
 
-    ! Get element coordinates on GL points (they include the edges that we use to calculate the element length)
-    CALL ChangeBasisVolume(3,PP_N,PP_N,Vdm_G_GL,Elem_xGP(:,:,:,:,ElemID),NodeCoords)
-
     ! Depending in the local sideID, get the edge vectors of the element in wall-normal direction (stored in yVec) and for the two
     ! wall-tangential directions (stored in tVec(:,1-2)). These vectors are the connection of the cell vertices, so they do not
     ! include any information about curvature!!!!
     SELECT CASE(locSideID)
     CASE(XI_MINUS,XI_PLUS)
-     yVec(:)   = NodeCoords(:,NGeo,0,0)- NodeCoords(:,0,0,0)
-     tVec(:,1) = NodeCoords(:,0,NGeo,0)- NodeCoords(:,0,0,0)
+     yVec(:)   = NodeCoords(:,NGeo,0,0,ElemID)- NodeCoords(:,0,0,,ElemID0)
+     tVec(:,1) = NodeCoords(:,0,NGeo,0,ElemID)- NodeCoords(:,0,0,,ElemID0)
 #if PP_dim==3
-     tVec(:,2) = NodeCoords(:,0,0,NGeo)- NodeCoords(:,0,0,0)
+     tVec(:,2) = NodeCoords(:,0,0,NGeo,ElemID)- NodeCoords(:,0,0,,ElemID0)
 #endif
     CASE(ETA_MINUS,ETA_PLUS)
-     yVec(:)   = NodeCoords(:,0,NGeo,0)- NodeCoords(:,0,0,0)
-     tVec(:,1) = NodeCoords(:,NGeo,0,0)- NodeCoords(:,0,0,0)
+     yVec(:)   = NodeCoords(:,0,NGeo,0,ElemID)- NodeCoords(:,0,0,,ElemID0)
+     tVec(:,1) = NodeCoords(:,NGeo,0,0,ElemID)- NodeCoords(:,0,0,,ElemID0)
 #if PP_dim==3
-     tVec(:,2) = NodeCoords(:,0,0,NGeo)- NodeCoords(:,0,0,0)
+     tVec(:,2) = NodeCoords(:,0,0,NGeo,ElemID)- NodeCoords(:,0,0,,ElemID0)
     CASE(ZETA_MINUS,ZETA_PLUS)
-     yVec(:)   = NodeCoords(:,0,0,NGeo)- NodeCoords(:,0,0,0)
-     tVec(:,1) = NodeCoords(:,NGeo,0,0)- NodeCoords(:,0,0,0)
-     tVec(:,2) = NodeCoords(:,0,NGeo,0)- NodeCoords(:,0,0,0)
+     yVec(:)   = NodeCoords(:,0,0,NGeo,ElemID)- NodeCoords(:,0,0,,ElemID0)
+     tVec(:,1) = NodeCoords(:,NGeo,0,0,ElemID)- NodeCoords(:,0,0,,ElemID0)
+     tVec(:,2) = NodeCoords(:,0,NGeo,0,ElemID)- NodeCoords(:,0,0,,ElemID0)
 #endif
     END SELECT
 
@@ -759,6 +753,5 @@ END DO ! iSide = 1,nBCSides
 
 END SUBROUTINE FillNonDimensionalGridSpacing
 #endif
-
 
 END MODULE MOD_EOS_Posti

18291fa08d0753cb58b62a7728c4d2ee0a5e73b7
Patrick Kopper <patrick.kopper@ila.uni-stuttgart.de>
2021-02-22 12:50:26 +0100

    Save meshHasMortars independent of existence of trees

--- src/mesh/mesh_readin.f90
+++ src/mesh/mesh_readin.f90
@@ -195,7 +195,7 @@ USE MOD_Mesh_Vars,          ONLY:NodeCoords,TreeCoords
 USE MOD_Mesh_Vars,          ONLY:offsetElem,offsetTree,nElems,nTrees,nGlobalTrees
 USE MOD_Mesh_Vars,          ONLY:xiMinMax,ElemToTree
 USE MOD_Mesh_Vars,          ONLY:nSides,nInnerSides,nBCSides,nMPISides,nAnalyzeSides
-USE MOD_Mesh_Vars,          ONLY:nMortarSides,isMortarMesh
+USE MOD_Mesh_Vars,          ONLY:nMortarSides,isMortarMesh,meshHasMortars
 USE MOD_Mesh_Vars,          ONLY:useCurveds
 USE MOD_Mesh_Vars,          ONLY:BoundaryType
 USE MOD_Mesh_Vars,          ONLY:MeshInitIsDone
@@ -715,6 +715,9 @@ CALL FinishCommunicateMeshReadin()
 CALL MPI_REDUCE(ReduceData,ReduceData_glob,10,MPI_INTEGER,MPI_SUM,0,MPI_COMM_FLEXI,iError)
 ReduceData=ReduceData_glob
 #endif /*USE_MPI*/
+
+! Store information if mesh has mortars, independent of existence of trees
+meshHasMortars = MERGE(.TRUE.,.FALSE.,ReduceData(9).GT.0)
 
 IF(MPIRoot)THEN
   WRITE(UNIT_stdOut,'(A,A34,I0)')' |','nElems               | ',ReduceData(1) !nElems

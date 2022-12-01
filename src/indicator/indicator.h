54999b97d70ac4fb24ad2203e0e2f5f43d52f6a3
Patrick Kopper <patrick.kopper@ila.uni-stuttgart.de>
2022-12-01 11:30:08 +0100

    Merge remaining changes back from particle.zles

---
+++ src/indicator/indicator.h
@@ -0,0 +1,24 @@
+!=================================================================================================================================
+! Copyright (c) 2010-2022  Prof. Claus-Dieter Munz
+! This file is part of FLEXI, a high-order accurate framework for numerically solving PDEs with discontinuous Galerkin methods.
+! For more information see https://www.flexi-project.org and https://nrg.iag.uni-stuttgart.de/
+!
+! FLEXI is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License
+! as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+!
+! FLEXI is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+! of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License v3.0 for more details.
+!
+! You should have received a copy of the GNU General Public License along with FLEXI. If not, see <http://www.gnu.org/licenses/>.
+!=================================================================================================================================
+
+#define INDTYPE_DG                  0
+#define INDTYPE_FV                  1
+#define INDTYPE_PERSSON             2
+#define INDTYPE_JAMESON             8
+#define INDTYPE_DUCROS              9
+#define INDTYPE_DUCROSTIMESJST      10
+#define INDTYPE_HALFHALF            3
+#define INDTYPE_CHECKERBOARD        33
+#define INDTYPE_BOX                 66
+#define INDTYPE_BLENDING_PERSSON    102

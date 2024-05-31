piclas-framework/piclas

Fixed bug in particle MPI emission: only set PEM%LastGlobalElemID=-1 for the current particle and not for the complete list.

src/particles/particle_mpi/particle_mpi_emission.f90
@@ -919,8 +919,8 @@ DO i = 1, chunkSize
       IF (TrackingMethod.EQ.REFMAPPING) THEN
         CALL GetPositionInRefElem(PartState(1:3,ParticleIndexNbr),PartPosRef(1:3,ParticleIndexNbr),ElemID)
       END IF ! TrackingMethod.EQ.REFMAPPING
-      PEM%GlobalElemID(ParticleIndexNbr)         = ElemID
-      PEM%LastGlobalElemID = -1 ! Initialize with invalid value
+      PEM%GlobalElemID(ParticleIndexNbr)     = ElemID
+      PEM%LastGlobalElemID(ParticleIndexNbr) = -1 ! Initialize with invalid value
       mySumOfMatchedParticles = mySumOfMatchedParticles + 1
     END IF ! ElemID.EQ.-1
   END IF ! InsideMyBGM(i)
@@ -1071,8 +1071,8 @@ DO i = 1,TotalNbrOfRecvParts
   IF (TrackingMethod.EQ.REFMAPPING) THEN
     CALL GetPositionInRefElem(PartState(1:3,ParticleIndexNbr),PartPosRef(1:3,ParticleIndexNbr),ElemID)
   END IF ! TrackingMethod.EQ.REFMAPPING
-  PEM%GlobalElemID(ParticleIndexNbr)    = ElemID
-  PEM%LastGlobalElemID = -1 ! Initialize with invalid value
+  PEM%GlobalElemID(ParticleIndexNbr)     = ElemID
+  PEM%LastGlobalElemID(ParticleIndexNbr) = -1 ! Initialize with invalid value
   mySumOfMatchedParticles = mySumOfMatchedParticles + 1
 END DO
 
@@ -1111,8 +1111,8 @@ DO iProc=0,PartMPIInitGroup(InitGroup)%nProcs-1
     IF (TrackingMethod.EQ.REFMAPPING) THEN
       PartPosRef(1:3,ParticleIndexNbr) = EmissionRecvBuf(iProc)%content(PartCommSize*(i-1)+4:PartCommSize*(i-1)+6)
     END IF ! TrackingMethod.EQ.REFMAPPING
-    PEM%GlobalElemID(ParticleIndexNbr)    = INT(EmissionRecvBuf(iProc)%content(PartCommSize*(i)),KIND=4)
-    PEM%LastGlobalElemID = -1 ! Initialize with invalid value
+    PEM%GlobalElemID(ParticleIndexNbr)     = INT(EmissionRecvBuf(iProc)%content(PartCommSize*(i)),KIND=4)
+    PEM%LastGlobalElemID(ParticleIndexNbr) = -1 ! Initialize with invalid value
     PDM%ParticleInside( ParticleIndexNbr) = .TRUE.
     mySumOfMatchedParticles = mySumOfMatchedParticles + 1
   END DO

flexi-framework/elexi

src/particles/mpi/particle_mpi_emission.f90
@@ -934,7 +934,8 @@ DO i = 1,TotalNbrOfRecvParts
   IF (TrackingMethod.EQ.REFMAPPING) THEN
     CALL GetPositionInRefElem(PartState(1:3,ParticleIndexNbr),PartPosRef(1:3,ParticleIndexNbr),ElemID)
   END IF ! TrackingMethod.EQ.REFMAPPING
-  PEM%Element(ParticleIndexNbr)         = ElemID
+  PEM%Element(ParticleIndexNbr)     = ElemID
+  PEM%lastElement(ParticleIndexNbr) = -1 ! Initialize with invalid value
 
   mySumOfMatchedParticles = mySumOfMatchedParticles + 1
   PDM%IsNewPart(ParticleIndexNbr) = .TRUE.
@@ -967,7 +968,8 @@ DO iProc=0,PartMPI%InitGroup(InitGroup)%nProcs-1
     IF (TrackingMethod.EQ.REFMAPPING) THEN
       PartPosRef(1:3,ParticleIndexNbr) = EmissionRecvBuf(iProc)%content(PartCommSize*(i-1)+4:PartCommSize*(i-1)+6)
     END IF ! TrackingMethod.EQ.REFMAPPING
-    PEM%Element(ParticleIndexNbr) = INT(EmissionRecvBuf(iProc)%content(PartCommSize*(i)),KIND=4)
+    PEM%Element(ParticleIndexNbr)     = INT(EmissionRecvBuf(iProc)%content(PartCommSize*(i)),KIND=4)
+    PEM%lastElement(ParticleIndexNbr) = -1 ! Initialize with invalid value
 
     PDM%ParticleInside( ParticleIndexNbr) = .TRUE.
 !     IF (TrackingMethod.EQ.REFMAPPING) THEN
3054f0ffdd1239061e4a02855df9ae2e3ab0e8ba
Patrick Kopper <kopper@iag.uni-stuttgart.de>
2025-04-28 16:19:57 +0200

    Linker: Empty is also valid

--- cmake/SetLinker.cmake
+++ cmake/SetLinker.cmake
@@ -6,7 +6,7 @@ IF(${CMAKE_VERSION} VERSION_LESS "3.29.0")
   RETURN()
 ENDIF()
 
-# Default is already set
+# Check if the linker type was set
 IF (CMAKE_LINKER_TYPE)
   # Use the standard linker, typically GNU ld
   IF ("${CMAKE_LINKER_TYPE}" STREQUAL "DEFAULT" OR "${CMAKE_LINKER_TYPE}" STREQUAL "SYSTEM")
@@ -30,6 +30,20 @@ IF (CMAKE_LINKER_TYPE)
   # Use the mold linker
   ELSEIF("${CMAKE_LINKER_TYPE}" STREQUAL "MOLD")
     EXECUTE_PROCESS(COMMAND mold     --version COMMAND grep "^mold"   COMMAND grep -Eo "[0-9]+\.[0-9]+\.[0-9]+ " OUTPUT_VARIABLE GNU_MOLD_VERSION)
+    STRING(STRIP "${GNU_MOLD_VERSION}" GNU_MOLD_VERSION)
+    MESSAGE(STATUS "Linking with [mold] (v${GNU_MOLD_VERSION})")
+  ENDIF()
+# Otherwise, default to (mo)ld
+ELSE()
+  EXECUTE_PROCESS(COMMAND ld       --version OUTPUT_VARIABLE GNU_LD_STRING)
+  # # Check if we really got GNU ld
+  IF (GNU_LD_STRING MATCHES "^GNU ld")
+    EXECUTE_PROCESS(COMMAND ld     --version COMMAND grep "^GNU ld" COMMAND sed "s/^.* //g"                    OUTPUT_VARIABLE GNU_LD_VERSION)
+    STRING(STRIP "${GNU_LD_VERSION}" GNU_LD_VERSION)
+    MESSAGE(STATUS "Linking with [ld] (v${GNU_LD_VERSION})")
+  # ... or if we are running mold
+  ELSEIF (GNU_LD_STRING MATCHES "^mold")
+    EXECUTE_PROCESS(COMMAND ld     --version COMMAND grep "^mold"   COMMAND grep -Eo "[0-9]+\.[0-9]+\.[0-9]+ " OUTPUT_VARIABLE GNU_MOLD_VERSION)
     STRING(STRIP "${GNU_MOLD_VERSION}" GNU_MOLD_VERSION)
     MESSAGE(STATUS "Linking with [mold] (v${GNU_MOLD_VERSION})")
   ENDIF()
